74f029226a0add2275f9d6bc2a88d515602d75ce
JorgeDoicela <ismael02doicela@gmail.com>
2026-09-15 09:22:25 -0500

    fix(frontend): configurar base path en '/' para resolucion correcta de assets en subdominio de produccion

--- dosier_web/vite.config.ts
+++ dosier_web/vite.config.ts
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
-    base: command === 'serve' ? '/' : '/dosier/',
+    base: '/',
     plugins: [
       react(),
       tailwindcss()
